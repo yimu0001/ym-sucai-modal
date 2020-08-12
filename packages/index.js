4832a660b57e452eb21efa4967eff2767ff94520
luanzixuan <yimuzx@foxmail.com>
2020-08-12 14:30:34 +0800

    修改导出

--- packages/index.js
+++ packages/index.js
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2020-08-11 11:18:22
- * @LastEditTime: 2020-08-11 11:38:14
- * @LastEditors: your name
+ * @LastEditTime: 2020-08-12 14:30:25
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \sucai-modal\packages\index.js
  */
@@ -10,4 +10,4 @@ import YMMateriesModal from '../src/components/sucai-modal.vue';
 YMMateriesModal.install = function(Vue) {
     Vue.component(YMMateriesModal.name, YQTreeSelector);
 };
-export default YMMateriesModal;
+export default {install, YMMateriesModal};
