a6ef9c5a48499cf3948e353154f1f845b1347f5d
luanzixuan <yimuzx@foxmail.com>
2020-08-12 15:14:05 +0800

    修改导出

--- vue.config.js
+++ vue.config.js
@@ -1,59 +1,57 @@
 /*
  * @Author: your name
  * @Date: 2020-07-24 09:12:36
- * @LastEditTime: 2020-08-12 14:11:42
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2020-08-12 15:07:26
+ * @LastEditors: your name
  * @Description: In User Settings Edit
  * @FilePath: \sucai-modal\vue.config.js
  */
-var webpack = require('webpack')
-const path = require('path')
-
-const resolve = dir => {
-  return path.join(__dirname, dir)
+const path = require('path');
+function resolve(dir) {
+    return path.resolve(__dirname, dir)
 }
 
-// 项目部署基础
-// 默认情况下，我们假设你的应用将被部署在域的根目录下,
-// 例如：https://www.my-app.com/
-// 默认：'/'
-// 如果您的应用程序部署在子路径中，则需要在这指定子路径
-// 例如：https://www.foobar.com/my-app/
-// 需要将它改为'/my-app/'
-// iview-admin线上演示打包路径： https://file.iviewui.com/admin-dist/
-const BASE_URL = process.env.NODE_ENV === 'production' ?
-  '/dist' :
-  ''
 module.exports = {
-  // Project deployment base
-  // By default we assume your app will be deployed at the root of a domain,
-  // e.g. https://www.my-app.com/
-  // If your app is deployed at a sub-path, you will need to specify that
-  // sub-path here. For example, if your app is deployed at
-  // https://www.foobar.com/my-app/
-  // then change this to '/my-app/'
-  publicPath: BASE_URL,
-  // tweak internal webpack configuration.
-  // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
-  // 如果你不需要使用eslint，把lintOnSave设为false即可
-  lintOnSave: false,
-  chainWebpack: config => {
-    config.resolve.alias
-      .set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
-      .set('_c', resolve('src/components'))
-      .set('_m', resolve('src/view/components/manuscripts'))
-  },
-  // 设为false打包时不生成.map文件
-  productionSourceMap: false,
-  // 这里写你调用接口的基础路径，来解决跨域，如果设置了代理，那你本地开发环境的axios的baseUrl要写为 '' ，即空字符串
-  devServer: {
-    proxy: 'http://sucai.shandian.design/'
-  },
-  pages: {
-    index: {
-      entry: 'src/main.js',
-      template: 'public/index.html',
-      filename: 'index.html'
-    }
-  }
+    pages: {
+        index: {
+            entry: 'src/main.js',
+            template: 'public/index.html',
+            filename: 'index.html',
+        },
+    },
+    productionSourceMap: false,
+    configureWebpack: {
+        output: {
+            libraryExport: 'default'
+        },
+        resolve: {
+            extensions: ['.js', '.vue', '.json'],
+            alias: {
+                '@': resolve('packages'),
+                'assets': resolve('src/assets'),
+                'views': resolve('src/views'),
+            }
+        }, 
+    },
+    devServer:{
+        port: 8091,
+        hot: true,
+        open: 'Google Chrome'
+    },
+    chainWebpack: config => {
+        config.module
+            .rule('js')
+            .include
+            .add('/packages')
+            .end()
+            .use('babel')
+            .loader('babel-loader')
+            .tap(options => {
+                return options
+            })
+    },
+    css: {
+        extract: false,
+    },
+
 }
