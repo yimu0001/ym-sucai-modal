ec052d97dae29698251b7b85f5abcc4eb51950d9
luanzixuan <yimuzx@foxmail.com>
2020-08-13 10:53:48 +0800

    修改jquery 引入

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2020-08-13 10:13:33
- * @LastEditTime: 2020-08-13 10:16:28
- * @LastEditors: your name
+ * @LastEditTime: 2020-08-13 10:53:00
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \ym-sucai-modal\src\components\index.js
  */
@@ -10,4 +10,4 @@ import YMMateriesModal from './sucai-modal.vue';
 YMMateriesModal.install = function(Vue) {
     Vue.component(YMMateriesModal.name, YMMateriesModal);
 };
-export default {install, YMMateriesModal};
+export default YMMateriesModal;
